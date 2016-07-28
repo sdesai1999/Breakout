a82a140453a11211cae76fb824c16b75b0a6d78b
Saurav Desai <esowmya@sauravs-macbook-pro-2.local>
2016-07-28 14:36:07 -0500

    got the ui to work just no brick removing and sliding

--- Breakout/ViewController.swift
+++ Breakout/ViewController.swift
@@ -20,7 +20,7 @@ class ViewController: UIViewController, UICollisionBehaviorDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        dynamicAnimator = UIDynamicAnimator(referenceView: view)
         // add a ball to the frame
         ball = UIView(frame: CGRectMake(view.center.x - 10, view.center.y, 20, 20))
         ball.backgroundColor = UIColor.blackColor()
@@ -33,29 +33,81 @@ class ViewController: UIViewController, UICollisionBehaviorDelegate {
         paddle.backgroundColor = UIColor.redColor()
         view.addSubview(paddle)
         
-        for j in 0..<8{
+        for j in 0..<10{
             for i in 0..<10{
-                bricks.append(UIView(frame: CGRectMake(CGFloat(40*i), CGFloat(20*j), 39, 19)))
+                let brick = UIView(frame: CGRectMake(CGFloat(i) * self.view.frame.width / 10, CGFloat(20*j), self.view.frame.width / 10, 20))
+                brick.layer.borderColor = UIColor.whiteColor().CGColor
+                brick.layer.borderWidth = 1
+                bricks.append(brick)
                 numBricks += 1
                 bricks[numBricks-1].backgroundColor = UIColor.redColor()
                 view.addSubview(bricks[numBricks-1])
-                if CGFloat(40*(i+1)+39) > view.frame.width{
-                    break
-                }
-                
             }
         }
         
-        dynamicAnimator = UIDynamicAnimator(referenceView: view)
+        for brick in bricks{
+            let brickDynamicBehavior = UIDynamicItemBehavior(items: [brick])
+            brickDynamicBehavior.density = 10000
+            brickDynamicBehavior.resistance = 100
+            brickDynamicBehavior.allowsRotation = false
+            dynamicAnimator.addBehavior(brickDynamicBehavior)
+        }
+        
+        
         
         // Create dynamic behavior for the ball
         let ballDynamicBehavior = UIDynamicItemBehavior(items: [ball])
         ballDynamicBehavior.friction = 0  // Collision resistance
-        ballDynamicBehavior.resistance = 0  // deceleration over time
+        ballDynamicBehavior.resistance = -0.025  // deceleration over time
         ballDynamicBehavior.elasticity = 1.0  // Bounce factor
         ballDynamicBehavior.allowsRotation = false
         dynamicAnimator.addBehavior(ballDynamicBehavior)
+        
+        // Create dynamic behavior for the paddle
+        let paddleDynamicBehavior = UIDynamicItemBehavior(items: [paddle])
+        paddleDynamicBehavior.density = 10000
+        paddleDynamicBehavior.resistance = 100
+        paddleDynamicBehavior.allowsRotation = false
+        dynamicAnimator.addBehavior(paddleDynamicBehavior)
+        
+        // Create a push behavior to get the ball moving
+        let pushBehavior = UIPushBehavior(items: [ball], mode: .Instantaneous)
+        pushBehavior.pushDirection = CGVectorMake(0.2, 1.0)
+        pushBehavior.magnitude = 0.25
+        dynamicAnimator.addBehavior(pushBehavior)
+        
+        
+        var item = bricks
+        item.append(paddle)
+        item.append(ball)
+        
+        // Create collision behaviors so ball can bounce off of other objects
+        collisionBehavior = UICollisionBehavior(items: item)
+        collisionBehavior.translatesReferenceBoundsIntoBoundary = true
+        collisionBehavior.collisionMode = .Everything
+        collisionBehavior.collisionDelegate = self
+        dynamicAnimator.addBehavior(collisionBehavior)
+        
     }
+    
+    /*
+    func collisionBehavior(behavior: UICollisionBehavior, beganContactForItem item1: UIDynamicItem, withItem item2: UIDynamicItem, atPoint p: CGPoint) {
+        for brick in bricks{
+            if (item1.isEqual(ball) && item2.isEqual(brick)) || (item1.isEqual(brick) && item2.isEqual(ball)) {
+                if brick.backgroundColor == UIColor.blueColor() {
+                    brick.backgroundColor = UIColor.yellowColor()
+                } else {
+                    brick.hidden = true
+                    collisionBehavior.removeItem(brick)
+                    //livesLabel.text = "You win!"
+                    ball.removeFromSuperview()
+                    collisionBehavior.removeItem(ball)
+                    dynamicAnimator.updateItemUsingCurrentState(ball)
+                }
+            }
+        }
+    }
+    */
     
     
 }
